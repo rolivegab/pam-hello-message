97897e128b37421b89ff45d5b6aef961a54c7864
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2020-01-30 13:18:25 -0300

    feat: dando tchau

---
+++ pages/bye.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Bye = () => (
+  <h2>Tchau! :(</h2>
+)
+
+export default Bye
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StyledDiv, GlobalStyle } from '../style'
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 const Index: NextPage = () => (
   <>
     <StyledDiv>
       Olá, Pamzinha!
     </StyledDiv>
+    <span style={{color: 'white'}}>Clique <Link href="bye" ><a style={{color: 'lightblue'}}>aqui</a></Link> para dar tchau</span>
     <GlobalStyle />
   </>
 )
